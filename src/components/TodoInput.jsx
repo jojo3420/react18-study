5cae9e8ae603d9c6b5c34c1e8099e35cb1b99c8f
jojo3420 <jjjhhhvvv@gmail.com>
2024-04-15 09:12:58 +0900

    - lint off - todo app 검포넌트 구성

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TodoInput(props) {
+	return (
+		<>
+			<button onClick={()=>{}}>clear</button>
+			<input type="text" />
+			<button onClick={()=>{}}>add</button>
+		</>
+	);
+}
+
+export default TodoInput;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import TodoInput from "./TodoInput.jsx";
+import TodoItem from "./TodoItem.jsx";
+import AllClearBtn from "./AllClearBtn.jsx";
+
+function TodoList() {
+	return (
+		<>
+			<h1>TodoList</h1>
+			<AllClearBtn />
+			<TodoInput />
+			{[1, 2, 3].map((value) => (
+				<TodoItem key={value} text={value} />
+			))}
+		</>
+	);
+}
+
+export default TodoList;
