9dce6801b28a1492472d41c87bb2bb19f2798f7b
jojo3420 <jjjhhhvvv@gmail.com>
2024-04-22 17:29:01 +0900

    Posts 렌더링 기능 구현

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -5,11 +5,11 @@ import User from '../users/User.jsx';
 
 function Post({ item }) {
   const [isExpanded, setIsExpanded] = useState(false);
-  const [isShowUserInfo, setIsShowUserInfo] = useState(false);
+  // const [isShowUserInfo, setIsShowUserInfo] = useState(false);
   const { isLoading, isError, data: postDetail, error } = useQuery({
     queryKey: ['post', item.id],
     queryFn: () => fetchPostById(item.id),
-    enabled: !!isExpanded,
+    enabled: isExpanded,
     staleTime: 1000 * 60 * 5,
     cacheTime: 1000 * 60 * 10,
   });
