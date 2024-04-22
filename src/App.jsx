671dc16b5cd23a9e15c725d12c77d6731425ceff
jojo3420 <jjjhhhvvv@gmail.com>
2024-04-22 14:42:14 +0900

    리액트 router 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import './App.css';
 import TodoList from './components/TodoList.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const Home = () => <h1>Home</h1>;
+
+
 
 function App() {
-  return <TodoList />;
+ return(
+   <BrowserRouter>
+   <Routes>
+    <Route path='/todolist' element={<TodoList />} />
+    <Route path='*' element={<Home />} />
+  </Routes>
+   </BrowserRouter>
+ );
+
 }
 
 export default App;
