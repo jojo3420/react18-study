f130017d0232723ab6d717c1a5e1feff04829ef4
jojo3420 <jjjhhhvvv@gmail.com>
2024-04-22 15:16:35 +0900

    Users 화면 기초 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import TodoList from './components/TodoList.jsx';
+import TodoList from './components/todolist/TodoList.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Users from './components/users/Users.jsx';
 
 const Home = () => <h1>Home</h1>;
-
 
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
    <BrowserRouter>
    <Routes>
     <Route path='/todolist' element={<TodoList />} />
+    <Route path='/users' element={<Users />} />
     <Route path='*' element={<Home />} />
   </Routes>
    </BrowserRouter>
---
+++ src/components/users/Users.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Users(props) {
+  return (
+    <ul>
+      <li>user1</li>
+      <li>user2</li>
+      <li>user3</li>
+    </ul>
+  );
+}
+
+export default Users;
